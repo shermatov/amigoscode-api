1094dcfb39fcd7c91f39da5792870122f6b15b4d
shermatov <aibekshermatovv@gmail.com>
2025-08-11 13:37:18 +0200

    CORS, Axios, FEtch Customers $ Card Components

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,67 @@
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+import {
+    Center,
+    Spinner,
+    Text,
+    Wrap,
+    WrapItem
+} from "@chakra-ui/react";
+
+import CartWithImage from "./components/Cart.jsx";
+
+function App() {
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true)
+
+        setTimeout(() => {
+            getCustomers().then((response) => {
+                setCustomers(response.data);
+            }).catch((error) => {
+                console.log(error);
+            }).finally(() => {
+                setLoading(false);
+            })
+        }, 1000)
+    }, [])
+
+    if (loading) {
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (<SidebarWithHeader>
+            <Text>
+                No Customers available
+            </Text>
+        </SidebarWithHeader>)
+    }
+
+    return (
+        <SidebarWithHeader>
+            <Wrap spacing='30px' justify='center'>
+                {customers.map((customer, index) => (
+                    <WrapItem key={index}>
+                        <CartWithImage {...customer}/>
+                    </WrapItem>
+                ))}
+            </Wrap>
+        </SidebarWithHeader>
+    )
+}
+
+export default App
